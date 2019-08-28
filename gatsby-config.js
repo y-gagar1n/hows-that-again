8e5b59992cc9c199196560c61fd978e8f2cd2d8e
y-gagar1n <y.timofeyev@gmail.com>
2019-08-28 08:35:06 +0300

    build search index on npm run build

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,28 @@ module.exports = {
                 }
             ]
         }
+    },
+    {
+        resolve: 'gatsby-plugin-lunr',
+        options: {
+            languages: [
+                { name: 'en' },
+                { name: 'ru' }
+            ],
+            fields: [
+                { name: 'title', store: true, attributes: { boost: 10 } },
+                { name: 'body', store: true },
+                { name: 'path', store: true }
+            ],
+            resolvers: {
+                MarkdownRemark: {
+                    title: node => node.frontmatter.title,
+                    body: node => node.rawMarkdownBody,
+                    path: node => node.fileAbsolutePath
+                }
+            },
+            filename: 'search_index.json'
+        }
     }
   ],
   pathPrefix: '/hows-that-again'
