57f7a6a8b9081283e49b530cc76f1fa2b09ccfff
Yury Timofeev <y.timofeyev@gmail.com>
2019-09-01 11:23:42 +0300

    add google search (#5)

--- src/components/header.js
+++ src/components/header.js
@@ -13,6 +13,8 @@ const Header = ({ siteTitle }) => (
         margin: '0 auto',
         maxWidth: 960,
         padding: '1.45rem 1.0875rem',
+        display: 'flex',
+        justifyContent: 'space-between'
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -26,6 +28,13 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <span style={{ margin: 'auto 0' }}>
+          <form style={{marginBottom: 0}} method="get" action="https://encrypted.google.com/search">
+              <input type="hidden" name="as_sitesearch" value="y-gagar1n.github.io/hows-that-again"/>
+              <input type="text" name="as_q"/>
+              <input style={{ marginLeft: 5 }} type="submit" value="Search"/>
+          </form>
+      </span>
     </div>
   </div>
 )
