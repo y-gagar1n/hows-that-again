c266e5aa56b51cb8dee8fc2ba0762dd4764c2135
heroku-publish <null>
2020-10-04 18:24:46 +0000

    Updates

--- component---src-pages-404-js-54ff81d98f14a7da0346.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{218:function(e,t,n){"use strict";n.r(t);var a=n(0),l=n.n(a),u=n(216);t.default=function(){return l.a.createElement(u.a,null,l.a.createElement("div",null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-54ff81d98f14a7da0346.js.map
